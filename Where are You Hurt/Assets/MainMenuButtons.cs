7eda6ba10ee088e1e4d83fdd62b5f8858bb927a3
JFlem <jordane.fleming.7@gmail.com>
2023-07-29 14:20:19 -0700

    working buttons

--- Where are You Hurt/Assets/MainMenuButtons.cs
+++ Where are You Hurt/Assets/MainMenuButtons.cs
@@ -1,8 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuButtons : MonoBehaviour
 {
-    
+    public void LoadMaleBody()
+    {
+        SceneManager.LoadScene("Body Male");
+    }
+
+    public void LoadFemaleBody()
+    {
+        SceneManager.LoadScene("Body Female");
+    }
 }
