d74f103780087f547e49d34cb99749328d534b3c
JFlem <jordane.fleming.7@gmail.com>
2023-07-29 16:15:04 -0700

    added working buttons on each body

--- Where are You Hurt/Assets/MainMenuButtons.cs
+++ Where are You Hurt/Assets/MainMenuButtons.cs
@@ -20,13 +20,61 @@ public class MainMenuButtons : MonoBehaviour
         SceneManager.LoadScene("Main Menu");
     }
 
-    public void LoadFrontRightArm()
+    //Front and Back Arms
+    public void FrontRightArm()
     {
         SceneManager.LoadScene("Front Right Arm");
     }
-
-    public void LoadFrontLeftArm()
+    public void FrontLeftArm()
     {
         SceneManager.LoadScene("Front Left Arm");
+    }
+    public void BackLeftArm()
+    {
+        SceneManager.LoadScene("Back Left Arm");
+    }
+    public void BackRightArm()
+    {
+        SceneManager.LoadScene("Back Right Arm");
+    }
+
+    //Front and Back Legs
+    public void FrontRightLeg()
+    {
+        SceneManager.LoadScene("Front Right Leg");
+    }
+    public void FrontLeftLeg()
+    {
+        SceneManager.LoadScene("Front Left Leg");
+    }
+    public void BackLeftLeg()
+    {
+        SceneManager.LoadScene("Back Left Leg");
+    }
+    public void BackRightLeg()
+    {
+        SceneManager.LoadScene("Back Right Leg");
+    }
+
+    //Others
+    public void FrontHead()
+    {
+        SceneManager.LoadScene("Front Head");
+    }
+    public void BackHead()
+    {
+        SceneManager.LoadScene("Back Head");
+    }
+    public void FemaleTorso()
+    {
+        SceneManager.LoadScene("Female Torso");
+    }
+    public void MaleTorso()
+    {
+        SceneManager.LoadScene("Male Torso");
+    }
+    public void BackSpine()
+    {
+        SceneManager.LoadScene("Back Spine");
     }
 }
