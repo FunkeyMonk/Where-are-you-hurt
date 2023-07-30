d2d838c42c46380f7a9afd6769c5a5af642bf17f
JFlem <jordane.fleming.7@gmail.com>
2023-07-30 13:30:54 -0700

    added functions

--- Where are You Hurt/Assets/ImageGenerator.cs
+++ Where are You Hurt/Assets/ImageGenerator.cs
@@ -6,9 +6,13 @@ using TMPro;
 
 public class ImageGenerator : MonoBehaviour
 {
+    //Front Head
     public Sprite eyes;
     public Sprite forehead;
     public Sprite ears;
+    public Sprite nose;
+    public Sprite mouth;
+    public Sprite neck;
 
     public TMP_Text bodyPartText;
 
@@ -19,11 +23,43 @@ public class ImageGenerator : MonoBehaviour
     {
         
     }
+
+    //----------------Front Head----------------------
     public void Eyes()
     {
         sliderThingy.SetActive(true);
         image.GetComponent<Image>().sprite = eyes;
         bodyPartText.text = "Eyes";
+    }
+    public void Forehead()
+    {
+        sliderThingy.SetActive(true);
+        image.GetComponent<Image>().sprite = forehead;
+        bodyPartText.text = "Forehead";
+    }
+    public void Ears()
+    {
+        sliderThingy.SetActive(true);
+        image.GetComponent<Image>().sprite = ears;
+        bodyPartText.text = "Ears";
+    }
+    public void Nose()
+    {
+        sliderThingy.SetActive(true);
+        image.GetComponent<Image>().sprite = nose;
+        bodyPartText.text = "Nose";
+    }
+    public void Mouth()
+    {
+        sliderThingy.SetActive(true);
+        image.GetComponent<Image>().sprite = mouth;
+        bodyPartText.text = "Mouth";
+    }
+    public void Neck()
+    {
+        sliderThingy.SetActive(true);
+        image.GetComponent<Image>().sprite = neck;
+        bodyPartText.text = "Neck";
     }
 
 }
