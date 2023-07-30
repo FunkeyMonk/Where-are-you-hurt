e3761155916ac3a9a2579db9b6176ca617b4ecc1
JFlem <jordane.fleming.7@gmail.com>
2023-07-30 12:41:12 -0700

    Update ImageGenerator.cs

--- Where are You Hurt/Assets/ImageGenerator.cs
+++ Where are You Hurt/Assets/ImageGenerator.cs
@@ -6,7 +6,7 @@ using TMPro;
 
 public class ImageGenerator : MonoBehaviour
 {
-    public Sprite eyes; // I attched these from editor
+    public Sprite eyes;
     public Sprite forehead;
     public Sprite ears;
 
@@ -23,34 +23,5 @@ public class ImageGenerator : MonoBehaviour
         image.GetComponent<Image>().sprite = eyes;
         bodyPartText.text = "Eyes";
     }
-
-    /*
-    public int imgNumberCount;
-
-
-    public void changeImages() // make sure to attach this to event trigger
-    {
-        switch (imgNumberCount)
-        {
-
-            case 0:
-                GetComponent<Image>().sprite = eyes;
-                imgNumberCount++; //increase count so it gets higher and switches to different sprite
-                break;
-            case 1:
-                GetComponent<Image>().sprite = forehead;
-                imgNumberCount++;
-                break;
-            case 2:
-                GetComponent<Image>().sprite = ears;
-                imgNumberCount++;
-                imgNumberCount = 0; //Reset it to 0
-                break;
-            default:
-                Debug.Log("Error");
-                break;
-        }
-    }
-    */
 
 }
